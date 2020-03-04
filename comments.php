f94a66e0b9ea69fd1968435c28de9eacd1d48a6c
geewizz246 <geewizz246@users.noreply.github.com>
2020-03-04 15:47:34 -0400

    Initial commit

---
+++ comments.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
+<body>
+ <?php
+	date_default_timezone_set('America/Barbados');
+	$theID = $_GET['id'];
+	require_once "vendor/autoload.php";
+	/*<<MONGODB PHP LIBRARY CODE GOES HERE>>
+		1.	Connect to the "Movies" collection in the "Original_Video" database
+		2.	Find one record with "_id" field equals to "new MongoDB\BSON\ObjectID($theID)"
+		3.	Get "comments" value and store it in "$comCount"
+	*/
+			
+?>
+<div class="container_50">
+	<div id='title'>
+		<a href='details.php?id=<?php echo $theID; ?>'>&lt;&lt;&nbsp;&nbsp;&nbsp;&nbsp;</a>Add or View Comments		
+	</div>
+   <div class="inner">
+   <form action="<?php $_SERVER['PHP_SELF'];?>" method="post" >
+    <div class="row">
+	  <div class="col-25">
+	  <?php
+		if (isset($item['PosterPath']))
+		{
+			$img = $item['PosterPath'];
+			echo "<img src=$img width='90%' height='225'/>";
+		}
+		else
+		{
+			echo "<img src='no_poster.png' width='90%' height='225'/>";
+			$title = $item['title'];
+			echo "<font color='green' size='6'>$title</b></font>";
+		}
+			
+		?>
+	  </div>
+	<div class="col-75">
+	  <div class="col-25">
+		<label for="iUser">Your Name</label>      
+	  </div>
+	  <div class="col-75">
+		 <input type="text" id="iUser" name="User" placeholder="Your Name..."  autofocus>
+	  </div>
+	  <div class="row">				
+	  </div>
+	  <div class="col-25">
+		<label for="iComment">Your Comment</label> 		
+	  </div>
+	  <div class="col-75">
+		<textarea name="comment" rows="9"> </textarea>
+	  </div>
+	</div>
+	  
+  </div>
+	  <div class="row">				
+		<br><input type="submit" value="Submit">
+	   </div>
+    
+  </form>
+   </div>
+  </div>
+  <?php
+		require_once "vendor/autoload.php";		
+		$comment = (isset($_POST['comment']) ? $_POST['comment'] : null );
+		$username = (isset($_POST['User']) ? $_POST['User'] : null );
+		if ((strlen(trim($comment)) != 0) && (!empty($username)))
+		{
+			$theArr = ['userName' => $username]; 
+			$theArr['comment'] = $comment; 		
+			$theArr['parentID'] = new MongoDB\BSON\ObjectID($theID); 	
+			$theArr['dateTime'] = date('M,d,Y h:i:s A');
+			
+			/*<<MONGODB PHP LIBRARY CODE GOES HERE>>
+				1.	Connect to the "Comments" collection in the "Original_Video" database
+				2.	Insert the "$theArr" into the "Comments" collection
+				3.	Increment the "comments" field of the "Movies" collection form "_id" field equals "new MongoDB\BSON\ObjectID($theID)"
+				4.	Get the "comments" value from "Movies" collection after the update, store the amount in "$comCount"
+			*/
+			
+		}			
+		if ($comCount > 0)	
+		{
+		    
+			$page  = isset($_GET['zpage']) ? (int) $_GET['zpage'] : 1;
+			$limit = 10; 	
+			$start_movement = 5; 
+			$skip  = ($page - 1) * $limit; 
+			$next  = ($page + 1);
+			$prev  = ($page - 1);
+			$range = 5; 	
+			$start_offset = ceil($range / 2); 
+			
+			
+			
+			
+			/*<<MONGODB PHP LIBRARY CODE GOES HERE>>
+				1.	Connect to the "Comments" collection in the "Original_Video" database
+				2.	Get all records with "parentID" field equals to "new MongoDB\BSON\ObjectID($theID)]" sorted by "_id" in descending order, limit by "$limit" and skip by "$skip"
+				3.	Count number of records for "parentID" field equals to "new MongoDB\BSON\ObjectID($theID)" and store value in "$total"			
+			*/
+						
+			
+			
+			
+			
+			$total_num_pages = ceil($total / $limit); 				
+			echo "<div class='bar'><b>".$comCount." Previous Comments</b><br><br><br>";
+			foreach ($result as $entry) 
+			{
+				$id =  $entry['_id'];
+				$userName = isset($entry['userName']) ? $entry['userName'] : '';
+				$comment = isset($entry['comment']) ? $entry['comment'] : '';
+				$date = isset($entry['dateTime']) ? $entry['dateTime'] : '';
+				echo "<div class='comm_title'>";
+				echo "<font size='4'><b>$userName</b></font>";
+				echo "<div style='float: right'>";
+					echo "<font size='2'>".$date."</font>";
+				echo "</div>";
+				echo "</div>";
+				echo "<div class='comm'>";
+					echo "$comment";			
+				echo "</div>";
+				echo "<br>";
+			}	
+			echo "</div>";
+			echo "<div class='pagination'>";	
+			if ($page >= $start_movement)
+			{
+				$start = $page-$start_offset;
+			}
+			else
+			{
+				$start = 1;
+			}
+			if ($page != 1)
+			{
+				echo '<a href="?zpage='.$prev.'&id='.$theID.'">Previous</a>';
+			}
+			if (($page < $total_num_pages - $start_offset) && ($total_num_pages > $start+$range))
+			{
+				for ($x=$start;$x<=$start+$range;$x++)
+				{	
+					if ($x > 0)
+					{
+						echo ' <a href="?zpage='.$x.'&id='.$theID.'" class = '. ($page == $x ? "active" : "").'>'.$x.'</a>';
+					}
+				}
+			}
+			else
+			{
+				for ($x=$total_num_pages-$range;$x<=$total_num_pages;$x++)
+				{
+					if ($x > 0)
+					{
+						echo ' <a href="?zpage='.$x.'&id='.$theID.'"  class = '. ($page == $x ? "active" : "").'>'.$x.'</a>';
+					}
+				}	
+			}
+			if($page * $limit < $total) 
+			{
+				echo ' <a href="?zpage='.$next.'&id='.$theID.'">Next</a>';
+			}
+			echo "</div>";
+			
+		}	
+  ?>
+
---
+++ posters.php
@@ -0,0 +1,57 @@
+<?php	
+	session_start();	
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="mystyle.css">
+	
+</head>
+<body>
+
+ <?php
+	require_once "vendor/autoload.php";
+	$page  = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+	$limit = 20; 	
+	$start_movement = 7; 
+	$skip  = ($page - 1) * $limit; 
+	$next  = ($page + 1);
+	$prev  = ($page - 1);
+	$range = 10; 		
+	$start_offset = ceil($range / 2); 	
+	$_SESSION["BackLink"] = "posters.php?page=$page";
+	
+	/*<<MONGODB PHP LIBRARY CODE GOES HERE>>
+		1.	Connect to the "Movies" collection in the "Video" database
+		2.	Find all documents that "PosterPath" exists, "year" greater than 1990, sorted by "year" in decesending order, where limit is "$limit", skip by "$skip" and store results in "$result"
+		3.	Count number of documents where "PosterPath" exists and "year" greater than 1990, and store result in "$total"
+	*/
+	
+	$collection = (new MongoDB\Client)->Original_Video->Movies;
+    $result = $collection->find( ['PosterPath' => ['$exists' => true], 'year' => ['$gt' => 1000] ], ['sort' => ['year' => -1], 'limit' => $limit , 'skip' => $skip],['projection' => ['PosterPath' => 1, '_id' => 1]]);
+	$total = $collection->count( [ 'PosterPath' => ['$exists' => true], 'year' => ['$gt' => 1990] ]);
+	
+	$total_num_pages = ceil($total / $limit); 
+	include 'pagenation.php';
+	echo "<div class='container_80'>";
+		echo "<div id='title'>";
+		echo "<a href='index.html'> <<&nbsp;&nbsp;&nbsp;&nbsp;</a>Movie with posters";
+		echo "</div>";
+		foreach ($result as $entry) {
+			$id =  $entry['_id'];
+			$pic = $entry['PosterPath'];
+			echo "<a href='details.php?id=$id'><div class='cardview'><img src='$pic'/></div></a>";		
+		}
+	echo "</div>";
+	include "pagenation.php";
+ ?>
+
+</body>
+</html>
+
+
+
+
+
+
