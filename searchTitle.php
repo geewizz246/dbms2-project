f935d667e26ebd6b1f209ddc17d6f49478882b92
Zary Sutherland <zaryafs@gmail.com>
2020-03-25 13:05:55 -0400

    Update: created function to get partial titles

--- searchTitle.php
+++ searchTitle.php
@@ -1,4 +1,13 @@
-<?php?>
+<?php	
+	session_start();
+
+	function searchDatabase($searchParam, $dbCollection){
+		if(is_string($searchParam)){
+			$result = $dbCollection->find( ['title' => ['$regex' => $searchParam]]);
+			return $result;
+		}
+	}	
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,7 +17,21 @@
 </head>
 <body>
 
+<?php
+	
+	require_once "vendor/autoload.php";
+	$page  = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+	$limit = 20; 	
+	$start_movement = 7; 
+	$skip  = ($page - 1) * $limit; 
+	$next  = ($page + 1);
+	$prev  = ($page - 1);
+	$range = 10; 		
+	$start_offset = ceil($range / 2); 	
+	$_SESSION["BackLink"] = "searchTitle.php?page=$page";
 
+	$collection = (new MongoDB\Client)->Original_Video->Movies;
 
+?>
 </body>
 </html>
