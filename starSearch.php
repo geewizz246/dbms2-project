7fe93c1dc5c0c806b5537cf6107fce942cc6ea28
Zary Sutherland <zaryafs@gmail.com>
2020-03-31 12:36:41 -0400

    Update: created function to search for star by name

--- starSearch.php
+++ starSearch.php
@@ -1,4 +1,17 @@
-<?php?>
+<?php	
+	session_start();
+
+	function searchDatabase($searchParam, $dbCollection, $pos){
+		if(is_string($searchParam)){
+			$cast='cast';
+			if($pos != '$'){
+				$cast= 'cast.'.$pos;
+			}
+			$result = $dbCollection->find( [$cast => ['$regex' => $searchParam]]);
+			return $result;
+		}
+	}	
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +20,27 @@
 	
 </head>
 <body>
+<?php
+	
+	require_once "vendor/autoload.php";
+	$page  = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+	$limit = 20; 	
+	$start_movement = 7; 
+	$skip  = ($page - 1) * $limit; 
+	$next  = ($page + 1);
+	$prev  = ($page - 1);
+	$range = 10; 		
+	$start_offset = ceil($range / 2); 	
+	$_SESSION["BackLink"] = "searchTitle.php?page=$page";
 
+	$collection = (new MongoDB\Client)->Original_Video->Movies;
+
+	// $result = searchDatabase('Denzel', $collection, '$');
+	// foreach ($result as $entry) {
+	// 	echo '<pre>', var_dump($entry['cast']) ,'</pre>';
+	// }
+
+?>
 
 
 </body>
